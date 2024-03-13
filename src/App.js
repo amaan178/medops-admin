a85c556ae720075457ae3aaf5b655f11d7e0e91b
Amaan Jambura <amaanjambura786@gmail.com>
2024-03-13 22:43:52 +0530

    Created and designed sidebar for the website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useStateContext } from './contexts/ContextProvider';
+
+import Sidebar from './components/Sidebar';
 
 const App = () => {
+  const {currentMode, activeMenu} = useStateContext();
   return (
-    <div>App</div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+         </div>
+      </BrowserRouter>
+    </div>  
   )
 }
 
